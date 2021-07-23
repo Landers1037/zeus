ba05ebbbbc2e82ad845dd5fb0ca64dcb19a4bf3d
landers <liaorenj@gmail.com>
2021-07-23 21:32:18 +0800

    blog启动方式变更

--- service/blog/start.sh
+++ service/blog/start.sh
@@ -6,6 +6,6 @@ cd ${ZEUS_APP_ROOT}/blog || exit 1
 for p in {10005..10009}
 do
   echo "启动监听$p"
-  nohup ./app_blog -p $p > ${ZEUS_APP_LOG}/blog.log 2>&1 &
+  nohup ./app_blog web cluster -p $p > ${ZEUS_APP_LOG}/blog.log 2>&1 &
 done
 exit 0
